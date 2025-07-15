f2a00bdfda730fa672fb71d2bb786bf517ad7ca9
Montaser Mohamed <montaser.seliem@gmail.com>
2025-07-15 20:44:18 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ echo "Copying index.html to build directory..."
 mkdir -p build
 cp index.html build/
 
-echo "$DATE"
+echo $USR
 echo "Build complete. Output is in ./build/"
 echo "A7la mesa 3la oyonak"
